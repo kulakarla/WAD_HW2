57808fceefb338839f1ec298d98f7423d17eebd0
kulakarla <sderrtsee@gmail.com>
2022-10-30 20:47:07 +0200

    finaldeed

--- src/js/script.js
+++ src/js/script.js
@@ -71,30 +71,17 @@ window.onload = function() {
         })
 
     
-    //document.getElementById("drop").addEventListener("click", myFunction);
-
     function myFunction() {
         console.log("tont");
-        document.getElementById("myDropdown").classList.toggle("show");
-    }
-    
-    window.onclick = function(event) {
-        if (!event.target.matches('.dropbtn')) {
-            var dropdowns = document.getElementsByClassName("dropdown-content");
-            var i;
-            for (i = 0; i < dropdowns.length; i++) {
-                var openDropdown = dropdowns[i];
-                if (openDropdown.classList.contains('show')) {
-                    openDropdown.classList.remove('show');
-                }
-            }
+        var x = document.getElementById("myDropdown");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+          } else {
+            x.style.display = "none";
         }
     }
 
-    //function showDropDown() {
-    //    document.getElementById("myDropdown").classList.toggle("show")
-    //}
-
+    
     window.myFunction = myFunction;
 
 
